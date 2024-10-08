f3e86c593cf3046c41c4032802bce97ebb1617e0
joaovitorvasc.2307 <joaovitorvasc.2307@gmail.com>
2024-10-08 18:18:50 +0000

    corigindo pagamentoRoutes

--- app/routes/pagamentoRoutes.js
+++ app/routes/pagamentoRoutes.js
@@ -2,47 +2,18 @@ const express = require('express');
 const router = express.Router();
 const PagamentoModel = require('./../models/pagamentoModel');
 
-// Configurar o Mercado Pago
-const mp = new MercadoPagoConfig({
-  accessToken: process.env.MERCADO_PAGO_ACCESS_TOKEN,
-});
-
-// Rota para criar um pedido e uma preferência de pagamento
-router.post('/criar-pedido', async (req, res) => {
-  const { idCliente, statusPagamento, idPagamento, carrinho } = req.body;
-
+router.get('/criar-pedido/:id_pedido', async (req, res) => {
   try {
-    // Criar o pedido no banco de dados
-    const pedidoId = await pedidoModel.criarPedido(idCliente, statusPagamento, idPagamento, carrinho);
-
-    // Calcular o preço total do carrinho
-    const precoTotal = carrinho.reduce((total, item) => total + (parseFloat(item.preco) * item.quantidade), 0);
-
-    // Criar a preferência de pagamento
-    const preference = {
-      items: carrinho.map(item => ({
-        title: item.titulo, // Título do item
-        quantity: item.quantidade, // Quantidade do item
-        unit_price: parseFloat(item.preco), // Preço unitário
-      })),
-      back_urls: {
-        success: 'http://www.your-site/success',
-        failure: 'http://www.your-site/failure',
-        pending: 'http://www.your-site/pending'
-      },
-      auto_return: 'approved',
-      payment_methods: {
-        excluded_payment_types: [{
-          id: 'ticket'
-        }],
-        installments: 6
-      }
-    };
-
-    const preferenceResponse = await mp.preferences.create(preference);
+    const { id_pedido } = req.params;
+    
+    // Chama a função e aguarda o retorno
+    const pagamento = await PagamentoModel.createPreferenceMP(id_pedido);
+    
+    // Extrai o valor do init_point do objeto de pagamento retornado
+    const initPoint = pagamento.id;
     
     // Faz o redirecionamento para o init_point
-    res.redirect(initPoint);
+    console.log(initPoint);
     
   } catch (error) {
     console.error('Erro ao criar preferência de pagamento:', error);
